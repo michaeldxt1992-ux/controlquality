f36b2af3aa96ca1202196a81e1703f081fb3ac47
michaeldxt1992-ux <michaeldxt1992@gmail.com>
2025-11-15 09:14:49 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,14 @@
-// ================================
-// CONFIGURAÇÃO FIREBASE (SDK 8)
-// ================================
-
+// firebase.js (SDK v8)
 var firebaseConfig = {
-    apiKey: "AIzaSyB2TRTp8vwXdvP4eMzrxmjmWKQcxPNhgRs",
-    authDomain: "controlquality-28980.firebaseapp.com",
-    projectId: "controlquality-28980",
-    storageBucket: "controlquality-28980.appspot.com",
-    messagingSenderId: "414926471614",
-    appId: "1:414926471614:web:9cd7527cd8e725f1c4f9ef"
+  apiKey: "AIzaSyB2TRTp8vwXdvP4eMzrxmjmWKQcxPNhgRs",
+  authDomain: "controlquality-28980.firebaseapp.com",
+  projectId: "controlquality-28980",
+  storageBucket: "controlquality-28980.appspot.com",
+  messagingSenderId: "414926471614",
+  appId: "1:414926471614:web:9cd7527cd8e725f1c4f9ef"
 };
-
-// Inicializar
+// inicializa
 firebase.initializeApp(firebaseConfig);
-
-// Exportar globalmente
+// exporta para uso global
 window.auth = firebase.auth();
 window.db = firebase.firestore();
