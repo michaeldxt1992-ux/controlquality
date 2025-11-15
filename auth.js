83778fe28a04d52abbdcea36d420db142a17a168
michaeldxt1992-ux <michaeldxt1992@gmail.com>
2025-11-15 01:40:10 -0300

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,38 @@
+// Função de Login
+function login() {
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    if (!email || !password) {
+        alert("Preencha e-mail e senha!");
+        return;
+    }
+
+    auth.signInWithEmailAndPassword(email, password)
+        .then(() => {
+            window.location.href = "home.html"; // login OK
+        })
+        .catch(error => {
+            alert("Erro ao entrar: " + error.message);
+        });
+}
+
+// Função para criar conta
+function signup() {
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    if (!email || !password) {
+        alert("Preencha e-mail e senha!");
+        return;
+    }
+
+    auth.createUserWithEmailAndPassword(email, password)
+        .then(() => {
+            alert("Conta criada com sucesso!");
+            window.location.href = "index.html";
+        })
+        .catch(error => {
+            alert("Erro ao criar conta: " + error.message);
+        });
+}
