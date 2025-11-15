343efb98b7f08f06afa90bfe65b4af7b02dc990d
michaeldxt1992-ux <michaeldxt1992@gmail.com>
2025-11-15 02:41:40 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// ===============================================
+// CONFIGURAÇÃO DO FIREBASE (SDK 8 COMPATÍVEL COM GITHUB PAGES)
+// ===============================================
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB2TRpBvvkD4eMzrzmjwIKQxXNPngRs",
+    authDomain: "controlquality-28980.firebaseapp.com",
+    projectId: "controlquality-28980",
+    storageBucket: "controlquality-28980.appspot.com",
+    messagingSenderId: "414924671464",
+    appId: "1:414924671464:web:9cd7527cd8e725fc14c9ef"
+};
+
+// Inicializa Firebase (SDK 8)
+firebase.initializeApp(firebaseConfig);
+
+// Exporta instâncias globais
+window.auth = firebase.auth();
+window.db = firebase.firestore();
