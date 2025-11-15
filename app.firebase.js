fae6415258ac2c61c9c56f9d8b21629b07a4f57a
michaeldxt1992-ux <michaeldxt1992@gmail.com>
2025-11-14 22:14:08 -0300

    Update app.firebase.js

--- app.firebase.js
+++ app.firebase.js
@@ -1,11 +1,18 @@
 // app.firebase.js
-// Substitua os valores abaixo pelo seu config do Firebase
+
+// Configuração Firebase (COMPLETA)
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  // ...
+    apiKey: "AIzaSyB2TRPto8wKdvP4eMzrxjmWkQCxPNhgRS",
+    authDomain: "controlquality-28980.firebaseapp.com",
+    projectId: "controlquality-28980",
+    storageBucket: "controlquality-28980.appspot.com",
+    messagingSenderId: "414926471614",
+    appId: "1:414926471614:web:9cd7527cd8e725fc14cf9ef"
 };
+
+// Inicializa Firebase (modo compatível com seu código)
 firebase.initializeApp(firebaseConfig);
+
+// Autenticação e Firestore
 const auth = firebase.auth();
 const db = firebase.firestore();
