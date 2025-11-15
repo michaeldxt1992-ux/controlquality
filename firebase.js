cfcad1eaebd3d6c6af219d004611c0989e0d6a80
michaeldxt1992-ux <michaeldxt1992@gmail.com>
2025-11-15 02:17:26 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,11 @@
-// ================================
-// CONFIGURAÇÃO DO FIREBASE
-// ================================
+// CONFIGURAÇÃO DO FIREBASE (SDK 8 COMPATÍVEL COM GITHUB PAGES)
 const firebaseConfig = {
-    apiKey: "AIzaSyB2TRp8vxWdvpP4eMzrxmiWjKQcxPNhgRs",
-    authDomain: "michaeldxt1992-ux.github.io",
+    apiKey: "AIzaSyB2TRpBvwKvD4eMzzrwjimWjKQcXNPhgRs",
+    authDomain: "controlquality-28980.firebaseapp.com",
     projectId: "controlquality-28980",
     storageBucket: "controlquality-28980.appspot.com",
-    messagingSenderId: "414924671414",
-    appId: "1:414924671414:web:9cd7527cd8e7251fc4f9ef"
+    messagingSenderId: "414924671464",
+    appId: "1:414924671464:web:9cd7527cd8e725fc14c9ef"
 };
 
 // Inicializa Firebase (SDK 8)
