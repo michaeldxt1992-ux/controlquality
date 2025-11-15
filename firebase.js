bd4f21a3ac29bbb29c5b88b9d0386382e858a227
michaeldxt1992-ux <michaeldxt1992@gmail.com>
2025-11-15 00:28:42 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,16 @@
-// firebase config placeholder
+// Configuração do Firebase (copiada do Console corretamente)
+const firebaseConfig = {
+    apiKey: "AIzaSyB2RTp8wKVdpPAeMzrxjmjWkQCxNPhgRS",
+    authDomain: "controlquality-28980.firebaseapp.com",
+    projectId: "controlquality-28980",
+    storageBucket: "controlquality-28980.appspot.com",
+    messagingSenderId: "414924671414",
+    appId: "1:414924671414:web:9cd7527cd8e725f1c4f9ef"
+};
+
+// Inicialização
+firebase.initializeApp(firebaseConfig);
+
+// Atalhos importantes
+const auth = firebase.auth();
+const db = firebase.firestore();
