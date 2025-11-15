95f211a5522877512e66884e35f89c97243e47e5
michaeldxt1992-ux <michaeldxt1992@gmail.com>
2025-11-15 03:46:25 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,17 @@
-// ==============================================================
+// ==============================================
 // CONFIGURAÇÃO DO FIREBASE (SDK 8 + GitHub Pages)
-// ==============================================================
+// ==============================================
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB2TRTp8vwXdvP4eMzrxmjMWkQCxPNhgRs",
-    authDomain: "controlquality-28980.firebaseapp.com",
-    projectId: "controlquality-28980",
-    storageBucket: "controlquality-28980.appspot.com",
-    messagingSenderId: "414926471644",
-    appId: "1:414926471644:web:9cd7527cd8e725fc14c9ef"
+  apiKey: "AIzaSyB2TRTp8vwXdvP4eMzrzxmjwlKQxXNPhgRs",
+  authDomain: "controlquality-28980.firebaseapp.com",
+  projectId: "controlquality-28980",
+  storageBucket: "controlquality-28980.firebasestorage.app",
+  messagingSenderId: "414926471464",
+  appId: "1:414926471464:web:9cd7527cd8e725fc14c9ef"
 };
 
-// Inicializa Firebase
+// Inicializa Firebase (SDK 8)
 firebase.initializeApp(firebaseConfig);
 
 // Exporta instâncias globais
