4493a0730f9067c2442289e0eacc9bf097d11a98
michaeldxt1992-ux <michaeldxt1992@gmail.com>
2025-11-15 08:57:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
+// ==============================
+// CRIAR RECLAMAÇÃO
+// ==============================
 
+function createComplaint() {
+    let titulo = document.getElementById("titulo").value.trim();
+    let descricao = document.getElementById("descricao").value.trim();
+
+    if (titulo === "" || descricao === "") {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    db.collection("complaints")
+        .add({
+            titulo,
+            descricao,
+            resposta: "",
+            userId: auth.currentUser.uid,
+            status: "Aguardando resposta",
+            createdAt: firebase.firestore.FieldValue.serverTimestamp()
+        })
+        .then(() => {
+            alert("Reclamação enviada com sucesso!");
+            window.location.href = "complaint-list.html";
+        })
+        .catch(err => {
+            alert("Erro ao enviar: " + err.message);
+        });
+}
