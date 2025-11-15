d244850f839282851c5b2194403c20f16758f183
michaeldxt1992-ux <michaeldxt1992@gmail.com>
2025-11-15 05:29:27 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,25 @@
-// ================================
-// CONFIGURAÇÃO FIREBASE (SDK 8 COMPAT)
-// ================================
+<!-- Firebase SDKs -->
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-firestore.js"></script>
 
-const firebaseConfig = {
-    apiKey: "AIzaSyB2TRPto8wKdvP4eMzrxjmWkQCxPNhgRS",
+<script>
+// ================================
+// CONFIGURAÇÃO FIREBASE (SDK 8)
+// ================================
+var firebaseConfig = {
+    apiKey: "AIzaSyB2TRTp8vwXdvP4eMzrxmjmWKQcxPNhgRs",
     authDomain: "controlquality-28980.firebaseapp.com",
     projectId: "controlquality-28980",
     storageBucket: "controlquality-28980.appspot.com",
     messagingSenderId: "414926471614",
-    appId: "1:414926471614:web:9cd7527cd8e725fc14c9ef"
+    appId: "1:414926471614:web:9cd7527cd8e725f1c4f9ef"
 };
 
-// Inicializar Firebase
+// Inicializar
 firebase.initializeApp(firebaseConfig);
 
-// Exportar objetos globais
+// Exportar
 window.auth = firebase.auth();
 window.db = firebase.firestore();
+</script>
